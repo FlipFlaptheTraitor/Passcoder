570f89e0d62000c86466f9c7992a5833192227d8
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-01-07 19:20:45 -0500

    work done on 1/6 pswrd count func

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
 // Assignment code here
-
-
+var pswdLength = pswdPeram;
+var pswdPeram = function(){ var pswdLength = window.prompt("please enter a number between 8 and 128 to determine the length of password ");
+      if (pswdLength >=  129 || pswdLength <= 7){
+        pswdPeram(); //= window.prompt("please enter a number between 8 and 128 to determine the length of password ");
+      }  
+} 
+console.log(pswdLength);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +20,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+pswdPeram();
