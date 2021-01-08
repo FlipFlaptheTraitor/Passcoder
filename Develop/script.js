0fb180b28e51b5814d944ced0bde8d48056f81e6
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-01-08 00:07:19 -0500

    saving first less eficent method not using rando java

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,74 @@
-// Assignment code here
-var pswdLength = pswdPeram;
-var pswdPeram = function(){ var pswdLength = window.prompt("please enter a number between 8 and 128 to determine the length of password ");
-      if (pswdLength >=  129 || pswdLength <= 7){
-        pswdPeram(); //= window.prompt("please enter a number between 8 and 128 to determine the length of password ");
-      }  
-} 
-console.log(pswdLength);
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
+const specialCharacters = "&'()*+,-./:;<=>?@][\^_`{|}~";
+const generateButton = document.getElementById('generate')
+generateButton.addEventListener('click', writePassword)
 // Write password to the #password input
+//var pswdPeram = pswdLength ;
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+var passwordText = document.querySelector("#password");
+ passwordText.value = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-pswdPeram();
+// Prompts that come up after you click generate password
+function generatePassword() {
+var passwordLength = window.prompt("please enter a number between 8 and 128 to determine the length of password ");
+      if (passwordLength >=  129 || passwordLength <= 7|| passwordLength == "" ) {
+        return generatePassword();
+        //= window.prompt("please enter a number between 8 and 128 to determine the length of password ");
+      }
+  var numbers = confirm("Do you want numbers in your password?");
+  var lowerCases = confirm("Do you want lowercases in your password?");
+  var upperCases = confirm("Do you want uppercases in your password?");
+  var special = confirm("Do you want special characters in your password?");
+  // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 0;
+  // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSpecialCharacters = "";
+  // Generator functions**
+  var functionArray = {
+    getNumbers: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+    getLowerCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+    getUpperCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+    getSpecialCharacters: function() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+};
+  // Checks to make sure user selected ok for all and uses empty minimums from above
+  if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCount++;
+  }
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCount++;
+  }
+  if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCount++;
+  }
+  if (special === true) {
+    minimumSpecialCharacters = functionArray.getSpecialCharacters();
+    minimumCount++;
+  }
+  // empty string variable for the for loop below
+  var randomPasswordGenerated = "";
+  // loop getting random characters
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4);
+    randomPasswordGenerated += randomNumberPicked;
+  }
+  // to make sure characters are added to the password
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumSpecialCharacters;
+  return randomPasswordGenerated;
+}
