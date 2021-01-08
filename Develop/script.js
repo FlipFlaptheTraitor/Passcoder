e9fb266617e2954ff7eb02b3d8d8bd5b8edd4ffb
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-01-08 00:05:22 -0500

    atlernitive method using pre-written randomizer

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,28 @@
-// Assignment code here
-var pswdLength = pswdPeram;
-var pswdPeram = function(){ var pswdLength = window.prompt("please enter a number between 8 and 128 to determine the length of password ");
-      if (pswdLength >=  129 || pswdLength <= 7){
-        pswdPeram(); //= window.prompt("please enter a number between 8 and 128 to determine the length of password ");
-      }  
-} 
-console.log(pswdLength);
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+var generateButton = document.getElementById('generate');
+generateButton.addEventListener('click', function() {
+    generateNewPassword();
+});
+function generateNewPassword() {
+    var length = Number(prompt("How many characters would you like your password to be?"));
+    while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+    var uppers = confirm("Would you like to use uppercase letters?");
+    var lowers = confirm("Would you like to use lowercase letters?");
+    var numbers = confirm("Would you like to use numbers?");
+    var symbols = confirm("Would you like to use special characters?");
+    while (!uppers && !lowers && !numbers && !symbols) {
+    alert("You must select at least one character type!");
+    uppers = confirm("Would you like to use uppercase letters?");
+    lowers = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    symbols = confirm("Would you like to use special characters?");
+    }
+    var password = "";
+    var allowed = {};
+    if (uppers) password += rando(allowed.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM");
+    if (lowers) password += rando(allowed.lowers = "qwertyuiopasdfghjklzxcvbnm");
+    if (numbers) password += rando(allowed.numbers = "1234567890");
+    if (symbols) password += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
+    for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
+    document.getElementById("password").value = randoSequence(password).join("");
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-pswdPeram();
